d90a469a1a6d621b7b3d2ba3fbceb7c6748c9e63
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2023-10-20 11:48:27 +0600

    done

---
+++ conditional_statement_example/android/app/src/main/kotlin/com/example/conditional_statement_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.conditional_statement_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ conditional_statement_example/lib/main.dart
@@ -0,0 +1,51 @@
+// void main() {
+// // if (
+// // senin loginin aizirek jana parolun 0000 ) { ekinchi betke ot}
+// // else {logininiz je parolunuz tuura emes};
+
+// // if(2 > 1){
+// //   print(true);
+// // }else{false};
+
+// //  final double fajr = 6.30;
+// //  final double zuhr = 13.40;
+
+// //  if(fajr == 6.30){
+// //   print("I will go to the mosque");
+// //  }else{
+// //   print('fajr namaz at $fajr');
+// //  }
+
+//   final double fajr = 5.42;
+//   final double zuhr = 12.48;
+//   final double asr = 16.33;
+//   final double magrib = 18.17;
+//   final double isha = 19.46;
+//   final double tahajud = 2.10;
+
+//   if (5.42 == fajr) {
+//     print("fajr");
+//   } else if (12.48 == zuhr) {
+//     print("zuhr");
+//   } else if (16.33 == asr) {
+//     print("asr");
+//   } else if (18.17 == magrib) {
+//     print("magrib");
+//   } else if (19.46 == isha) {
+//     print("isha");
+//   } else if (2.10 == tahajud) {
+//     print("tahajud");
+//   }
+// }
+
+// void main() {
+//   final String login = "16 gruppa";
+//   final String password = '0000';
+
+//   if (login == '16 gruppa' && password == '0000') {
+//     print('ekinchi betke ot');
+//   } else {
+//     print('sizdin logininiz je parolunuz tuura emes');
+//   }
+// }
+
